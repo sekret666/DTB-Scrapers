2b4304c443123fe1d78f6765a1425f600b77251f
sekret666 <66795778+sekret666@users.noreply.github.com>
2021-04-18 15:50:51 +0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,71 @@
+<p align="center">
+  <img src="https://telegra.ph/file/d827dfc6b2da61667a5d4.jpg" width="300" height="300">
+</p>
+
+<p align="center"><img src="https://img.shields.io/badge/Version-3.1-brightgreen"></p>
+<p align="center">
+  <a href="https://github.com/aykhan026">
+    <img src="https://img.shields.io/github/followers/aykhan026?label=Follow&style=social">
+  </a>
+  <a href="https://github.com/aykhan026/RoBotlarimTg-Scraper">
+    
+  </a>
+
+<div align="center">
+  <h1>RoBotlarım 👨🏻‍💻</h1>
+  <h1>Scraper</h1>
+</div>
+
+---
+## Qeyd 
+Telegraph programının işləməməsi nəticəsində artıq 
+Telegramda user daşımaq minimum səviyyədədir və ancaq Azərbaycan nömrələriylə mümkündür
+Bu script vaistəsiyıə +1 (fake) nömrələr vaistəsiylədə user daşıya biıəcəksiniz.
+Tam Azərbaycan Dilindədir 🇦🇿
+# ⚠️DİQQƏT⚠️
+Qrupdan qrupa user daşıyaraq istifadəçiləri və eyni zamanda
+qrup sahiblərini narahat etmiş olursunuz !
+<a href="https://t.me/RoBotlarimTg">RoBotlarım👨🏻‍💻</a> bu narahatçılıqlara görə heçbir məsuliyyət daşımır !
+
+## • APİ almaq
+* Brauzerlə http://my.telegram.org adresinə daxil olun
+* Qeydiyyatdan keçin
+* APİ id və APİ Hash kopyalayıb bir yerə qeyd edin
+
+## • Quraşdırmaq
+* Termuxu açın və kodları növbəylə yazın
+
+`$ apt update`
+
+`$ pkg install python git`
+
+* "y" yazaraq təsdiqləyin
+
+`$ git clone https://github.com/aykhan026/RoBotlarimTg-Scraper.git`
+
+`$ cd RoBotlarimTg-Scraper`
+
+`$ python3 setup.py -i`
+
+* Açılan səhifədə "n" yazıb enter basın
+
+`$ python3 setup.py -c`
+
+* API-ID API-Hash və api aldığınız hesabın telefon nömrəsini düzgün qeyd edin
+
+`$ python3 scraper.py`
+
+* Telegrama gedən kodu daxil edin
+* Açılan səhifədə user götürməy istədiyiniz qrupu daxil edin
+
+`$ cat members.csv`
+
+* Userlər yükləndikdən sonra aşağıdaki kodu yazın
+
+`$ python3 add2group.py members.csv`
+
+* Və açılan səhifədə userləri atacağınız qrupu seçin
+---
+## 📣Əlavə
+* Əgər bu sizin işinizə yaradısa <a href="https://t.me/RoBotlarimTg">RoBotlarım👨🏻‍💻</a> kanalına abu olmağı unutmayın 😉
+* Qeyd edimki bu scripti işlətməklə heçbir şəxsi məlumatlarınız oğurlanmadığınnan tam əmin ola bilərsiniz açıq kodlardı gözdən keçirə bilərsiniz.
---
+++ add2group.py
@@ -0,0 +1,122 @@
+#!/bin/env python3
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty, InputPeerChannel, InputPeerUser
+from telethon.errors.rpcerrorlist import PeerFloodError, UserPrivacyRestrictedError
+from telethon.tl.functions.channels import InviteToChannelRequest
+import configparser
+import os, sys
+import csv
+import traceback
+import time
+import random
+
+re="\033[1;31m"
+gr="\033[1;32m"
+cy="\033[1;36m"
+
+def banner():
+    print(f"""
+*╔═══❖══════❖═══╗
+ ║ DTB ║
+ ║ Scraper ║
+ ║   ᕙ( ͡° ͜ʖ ͡°)ᕗ ║
+*╚═══❖══════❖═══╝""")
+
+cpass = configparser.RawConfigParser()
+cpass.read('config.data')
+
+try:
+    api_id = cpass['cred']['id']
+    api_hash = cpass['cred']['hash']
+    phone = cpass['cred']['phone']
+    client = TelegramClient(phone, api_id, api_hash)
+except KeyError:
+    os.system('clear')
+    banner()
+    print(re+"[!] əvvəlcə python3 setup.py başladın !!\n")
+    sys.exit(1)
+
+client.connect()
+if not client.is_user_authorized():
+    client.send_code_request(phone)
+    os.system('clear')
+    banner()
+    client.sign_in(phone, input(gr+'[+] Kodu daxil edin: '+re))
+ 
+os.system('clear')
+banner()
+input_file = sys.argv[1]
+users = []
+with open(input_file, encoding='UTF-8') as f:
+    rows = csv.reader(f,delimiter=",",lineterminator="\n")
+    next(rows, None)
+    for row in rows:
+        user = {}
+        user['username'] = row[0]
+        user['id'] = int(row[1])
+        user['access_hash'] = int(row[2])
+        user['name'] = row[3]
+        users.append(user)
+ 
+chats = []
+last_date = None
+chunk_size = 200
+groups=[]
+ 
+result = client(GetDialogsRequest(
+             offset_date=last_date,
+             offset_id=0,
+             offset_peer=InputPeerEmpty(),
+             limit=chunk_size,
+             hash = 0
+         ))
+chats.extend(result.chats)
+ 
+for chat in chats:
+    try:
+        if chat.megagroup== True:
+            groups.append(chat)
+    except:
+        continue
+ 
+i=0
+for group in groups:
+    print(gr+'['+cy+str(i)+gr+']'+cy+' - '+group.title)
+    i+=1
+
+print(gr+'[+] Üzvləri əlavə etmək üçün bir qrup seçin')
+g_index = input(gr+"[+] Qrup nömrəsini yazın : "+re)
+target_group=groups[int(g_index)]
+ 
+target_group_entity = InputPeerChannel(target_group.id,target_group.access_hash)
+ 
+print(gr+"[1] istifadəçi tərəfindən üzv əlavə edin ID\n[2] istifadəçi adı ilə üzv əlavə edin ")
+mode = int(input(gr+"Bura 1 yazın : "+re)) 
+n = 0
+ 
+for user in users:
+    n += 1
+    if n % 50 == 0:
+	    time.sleep(1)
+	    try:
+	        print ("Əlavə olunur {}".format(user['id']))
+	        if mode == 1:
+	            if user['username'] == "":
+	                continue
+	            user_to_add = client.get_input_entity(user['username'])
+	        elif mode == 2:
+	            user_to_add = InputPeerUser(user['id'], user['access_hash'])
+	        else:
+	            sys.exit(re+"[!] Yanlış rejim seçildi. Zəhmət olmasa bir daha cəhd edin.")
+	        client(InviteToChannelRequest(target_group_entity,[user_to_add]))
+	        print(gr+"[+] Spam olmaması üçün 5-10 saniyə gözləyirəm...")
+	        time.sleep(random.randrange(5, 10))
+	    except PeerFloodError:
+	        print(re+"[!] Telegramnan Spam Flood Xətası alərıq zəhmət olmasa başka profillə yenidən cəhd edin \n[!] Script dayandırıldı. \n[!] Bu bizim səhvimiz deyil Telegram tərəfindən bloklanmısınız.")
+	    except UserPrivacyRestrictedError:
+	        print(re+"[!] İstifadəçinin məxfilik parametrləri bunu etməyə imkan vermir. Bu istifadəçini keçdim.")
+	    except:
+	        traceback.print_exc()
+	        print(re+"[!] Gözlənilməz Xəta. Zəhmət olmasa ekran şəkli çəkib onu RoBotlarim Support qrupuna göndərin.")
+	        continue
